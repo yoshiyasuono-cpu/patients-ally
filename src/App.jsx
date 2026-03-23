ad1947e8d505a8006b3dd0fafbfe539d13e1110d
yoshiyasuono-cpu <yoshiyasuono@gmail.com>
2026-03-23 13:52:44 +0900

    fix: ページ遷移時にスクロールをトップに戻す

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import ClinicList from './pages/ClinicList';
 import ClinicDetail from './pages/ClinicDetail';
 import ConsultationForm from './pages/ConsultationForm';
 import ClinicAdmin from './pages/ClinicAdmin';
 import './index.css';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<ClinicList />} />
         <Route path="/clinic/:id" element={<ClinicDetail />} />
